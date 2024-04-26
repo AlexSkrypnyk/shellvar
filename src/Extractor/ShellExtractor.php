d37ecb04beebe25553c592468d2340990799fd61
Alex Skrypnyk <alex@drevops.com>
2024-04-27 08:49:55 +1000

    Extracted variable parser into own class.

--- src/Extractor/ShellExtractor.php
+++ src/Extractor/ShellExtractor.php
@@ -68,7 +68,7 @@ class ShellExtractor extends AbstractExtractor {
       $var->addPath($absolute_filepath);
 
       if ($var->getIsAssignment()) {
-        $default_value = $this->extractVariableValue($line, $this->config->get('unset'));
+        $default_value = VariableParser::parseValue($line, $this->config->get('unset'));
         // Assign a value, but not if it defaults to a variable name.
         if ($default_value && $default_value !== $var->getName()) {
           $var->setDefaultValue($default_value);
@@ -76,7 +76,7 @@ class ShellExtractor extends AbstractExtractor {
       }
 
       $description_prefix = is_array($this->config->get('skip-description-prefix')) ? $this->config->get('skip-description-prefix') : [];
-      $description = $this->extractVariableDescription($lines, $num, $description_prefix);
+      $description = VariableParser::parseDescription($lines, $num, $description_prefix, self::COMMENT_SEPARATOR);
       if (!empty($description)) {
         if ($skip && str_contains($description, $skip)) {
           continue;
@@ -141,173 +141,6 @@ class ShellExtractor extends AbstractExtractor {
     }
 
     return NULL;
-  }
-
-  /**
-   * Extract variable value from a line.
-   *
-   * It is already known that the line contains a variable assignment.
-   *
-   * @param string $line
-   *   A line to extract a variable value from.
-   * @param mixed $default_value
-   *   The default value to return if a value was not extracted.
-   *
-   * @return string|mixed
-   *   A variable value.
-   */
-  protected function extractVariableValue($line, mixed $default_value) {
-    [, $value] = explode('=', $line, 2);
-
-    $value = trim($value);
-
-    if (empty($value)) {
-      return $default_value;
-    }
-
-    // Validate value.
-    // Even number of quotes.
-    if (str_contains($value, '"') && substr_count($value, '"') % 2 !== 0) {
-      throw new \RuntimeException('Invalid number of quotes in the value: ' . $value);
-    }
-    // Even number of braces.
-    if ((str_contains($value, '{') || str_contains($value, '}')) && substr_count($value, '}') !== substr_count($value, '{')) {
-      throw new \RuntimeException('Unbalanced braces in the value: ' . $value);
-    }
-
-    // Replace all outermost matching patterns with the found sub-group. This
-    // allows to reduce the value to the innermost matching pattern.
-    while (TRUE) {
-      $replaced_count = 0;
-
-      $regex = '/\$\{(?:[^{}]*|\{[^{}]*\})*\$\{([^{}]+)\}(?:[^{}]*|\{[^{}]*\})*\}/';
-      $value = preg_replace_callback($regex, static function ($matches) use (&$replaced_count): string {
-        $to_replace = $matches[0];
-
-        $innermost = $matches[1] ?? NULL;
-        if (!$innermost) {
-          return '';
-        }
-
-        $parsed = self::parseVariableString($innermost);
-        if (!$parsed) {
-          return '';
-        }
-
-        $replace_with = trim($parsed['default'] ?? '$' . $parsed['name'], '"');
-        $replaced = str_replace('${' . $innermost . '}', $replace_with, $to_replace);
-
-        $replaced_count++;
-
-        return $replaced;
-      }, (string) $value);
-
-      if ($replaced_count === 0) {
-        break;
-      }
-    }
-
-    $value = trim((string) $value, '"');
-
-    if (str_starts_with($value, '$')) {
-      if (str_starts_with($value, '${')) {
-        $value = trim($value, '${}');
-        $value = trim($value, '"');
-
-        $parsed = self::parseVariableString($value);
-        if (!$parsed) {
-          return $default_value;
-        }
-
-        $value = trim($parsed['default'] ?? '$' . $parsed['name'], '"');
-      }
-
-      // Numeric values are script arguments, so we convert them to defaults.
-      $value = str_starts_with($value, '$') && is_numeric(trim($value, '$')) ? $default_value : $value;
-
-      $value = trim((string) $value, '$');
-    }
-
-    return empty($value) ? $default_value : $value;
-  }
-
-  /**
-   * Parse a variable string.
-   *
-   * @param string $string
-   *   A variable string to parse.
-   *
-   * @return array|null
-   *   An array representation of a parsed variable string with the following:
-   *   - name: The variable name.
-   *   - operator: The operator.
-   *   - default: The default value.
-   */
-  protected static function parseVariableString(string $string): ?array {
-    $parts = [
-      'name' => $string,
-      'operator' => NULL,
-      'default' => NULL,
-    ];
-
-    preg_match('/([^:+=?-]+)(-|:-|:=|\+=|\?)(.+)?/', $string, $matches);
-
-    if (empty($matches)) {
-      return $parts;
-    }
-
-    $parts = [
-      'name' => array_slice($matches, 1)[0] ?? $string,
-      'operator' => array_slice($matches, 1)[1] ?? NULL,
-      'default' => array_slice($matches, 1)[2] ?? NULL,
-    ];
-
-    if ($parts['operator'] === '?') {
-      $parts['default'] = NULL;
-    }
-
-    return $parts;
-  }
-
-  /**
-   * Extract variable description from multiple lines.
-   *
-   * @param array<string> $lines
-   *   A list of lines to extract a variable description from.
-   * @param int $line_num
-   *   A line number to start from.
-   * @param array<string> $skip_prefixes
-   *   A list of prefixes to skip.
-   * @param string $comment_separator
-   *   A comment delimiter.
-   *
-   * @return string
-   *   A variable description.
-   */
-  protected function extractVariableDescription(array $lines, $line_num, array $skip_prefixes = [], $comment_separator = self::COMMENT_SEPARATOR): string {
-    $comment_lines = [];
-
-    $line_num = min($line_num, count($lines) - 1);
-
-    // Look behind until the first non-comment line.
-    while ($line_num > 0 && str_starts_with(trim($lines[$line_num - 1]), $comment_separator)) {
-      $comment_lines[] = trim(ltrim(trim($lines[$line_num - 1]), $comment_separator));
-      $line_num--;
-    }
-
-    $comment_lines = array_reverse($comment_lines);
-
-    $comment_lines = array_filter($comment_lines, static function ($value) use ($skip_prefixes, $comment_separator): bool {
-      foreach ($skip_prefixes as $prefix) {
-        if (str_starts_with($value, ltrim($prefix, $comment_separator))) {
-          return FALSE;
-        }
-      }
-
-      return TRUE;
-    });
-
-    return implode("\n", $comment_lines);
   }
 
 }
---
+++ tests/phpunit/Unit/VariableParserUnitTest.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace AlexSkrypnyk\Shellvar\Tests\Unit;
+
+use AlexSkrypnyk\Shellvar\Extractor\VariableParser;
+
+/**
+ * Class ExtractorUnitTest.
+ *
+ * Unit tests for the Extractor class.
+ *
+ * phpcs:disable Drupal.Arrays.Array.LongLineDeclaration
+ *
+ * @coversDefaultClass \AlexSkrypnyk\Shellvar\Extractor\VariableParser
+ */
+class VariableParserUnitTest extends UnitTestBase {
+
+  /**
+   * Tests the parseValue() method.
+   *
+   * @dataProvider dataProviderParseValueNotation
+   * @covers ::parseValueNotation
+   */
+  public function testParseValueNotation(string $line, string $name, ?string $operator, ?string $default): void {
+    $extractor = $this->prepareMock(VariableParser::class, [], FALSE);
+    $actual = (array) $this->callProtectedMethod($extractor, 'parseValueNotation', [$line, 'TESTUNSET']);
+    $this->assertSame($name, $actual['name']);
+    $this->assertSame($operator, $actual['operator']);
+    $this->assertSame($default, $actual['default']);
+  }
+
+  /**
+   * Data provider for testParseValueNotation().
+   */
+  public static function dataProviderParseValueNotation(): array {
+    return [
+      ['', '', NULL, NULL],
+
+      ['VAR1', 'VAR1', NULL, NULL],
+
+      ['VAR1-', 'VAR1', '-', NULL],
+      ['VAR1:-', 'VAR1', ':-', NULL],
+      ['VAR1:=', 'VAR1', ':=', NULL],
+      ['VAR1+=', 'VAR1', '+=', NULL],
+      ['VAR1?', 'VAR1', '?', NULL],
+
+      ['VAR1-val', 'VAR1', '-', 'val'],
+      ['VAR1:-val', 'VAR1', ':-', 'val'],
+      ['VAR1:=val', 'VAR1', ':=', 'val'],
+      ['VAR1+=val', 'VAR1', '+=', 'val'],
+      // Special case for '?'.
+      ['VAR1?val', 'VAR1', '?', NULL],
+
+      ['VAR1-"val"', 'VAR1', '-', '"val"'],
+      ['VAR1:-"val"', 'VAR1', ':-', '"val"'],
+      ['VAR1:="val"', 'VAR1', ':=', '"val"'],
+      ['VAR1+="val"', 'VAR1', '+=', '"val"'],
+      // Special case for '?'.
+      ['VAR1?"val"', 'VAR1', '?', NULL],
+
+      ['VAR1-$VAR2', 'VAR1', '-', '$VAR2'],
+      ['VAR1:-$VAR2', 'VAR1', ':-', '$VAR2'],
+      ['VAR1:=$VAR2', 'VAR1', ':=', '$VAR2'],
+      ['VAR1+=$VAR2', 'VAR1', '+=', '$VAR2'],
+      // Special case for '?'.
+      ['VAR1?$VAR2', 'VAR1', '?', NULL],
+
+      ['VAR1-"$VAR2"', 'VAR1', '-', '"$VAR2"'],
+      ['VAR1:-"$VAR2"', 'VAR1', ':-', '"$VAR2"'],
+      ['VAR1:="$VAR2"', 'VAR1', ':=', '"$VAR2"'],
+      ['VAR1+="$VAR2"', 'VAR1', '+=', '"$VAR2"'],
+      // Special case for '?'.
+      ['VAR1?"$VAR2"', 'VAR1', '?', NULL],
+
+      // Negative.
+      ['VAR1=', 'VAR1=', NULL, NULL],
+      ['VAR1 =', 'VAR1 =', NULL, NULL],
+    ];
+  }
+
+  /**
+   * Tests the parseValue() method.
+   *
+   * @dataProvider dataProviderExtractVariableValue
+   * @covers ::parseValue
+   * @covers ::validateValue
+   * @covers ::resolveNestedNotations
+   * @group wip4
+   */
+  public function testExtractVariableValue(string $line, string|int $expected): void {
+    $actual = VariableParser::parseValue($line, 'TESTUNSET');
+    $this->assertSame($expected, $actual);
+  }
+
+  /**
+   * Data provider for testExtractVariable().
+   *
+   * Note that we only assert for assignment expressions. Non-assignment
+   * expressions would not reach this method.
+   */
+  public static function dataProviderExtractVariableValue(): array {
+    return [
+      ['VAR1=', 'TESTUNSET'],
+      ['VAR1= ', 'TESTUNSET'],
+      ['VAR1=   ', 'TESTUNSET'],
+      ['VAR1=""', 'TESTUNSET'],
+      ['VAR1= ""', 'TESTUNSET'],
+
+      ['VAR1=" "', ' '],
+      ['VAR1= " "', ' '],
+
+      ['VAR1=123', '123'],
+      ['VAR1="123"', '123'],
+
+      ['VAR1=$VAR2', 'VAR2'],
+      ['VAR1="$VAR2"', 'VAR2'],
+      ['VAR1=${VAR2}', 'VAR2'],
+      ['VAR1="${VAR2}"', 'VAR2'],
+
+      ['VAR1=${VAR2:-}', 'VAR2'],
+      ['VAR1="${VAR2:-}"', 'VAR2'],
+      ['VAR1=${VAR2:-123}', '123'],
+      ['VAR1="${VAR2:-123}"', '123'],
+
+      ['VAR1=${VAR2:-$VAR3}', 'VAR3'],
+      ['VAR1="${VAR2:-$VAR3}"', 'VAR3'],
+      ['VAR1="${VAR2:-"$VAR3"}"', 'VAR3'],
+      ['VAR1=${VAR2:-${VAR3}}', 'VAR3'],
+      ['VAR1=${VAR2:-"${VAR3}"}', 'VAR3'],
+      ['VAR1="${VAR2:-"${VAR3}"}"', 'VAR3'],
+      ['VAR1="${VAR2:-${VAR3}}"', 'VAR3'],
+      ['VAR1="${VAR2:-${VAR3:-}}"', 'VAR3'],
+      ['VAR1="${VAR2:-${VAR3:-567}}"', '567'],
+      ['VAR1="${VAR2:-${VAR3:-567}}"', '567'],
+      ['VAR1="${VAR2:-${VAR3:-"${VAR4:-567}"}}"', '567'],
+
+      // Still a valid expression in the context of this method.
+      ['$VAR1=123', '123'],
+      ['${VAR1}=123', '123'],
+
+      // Script arguments.
+      ['VAR1=${VAR2:-$1}', 'TESTUNSET'],
+      ['VAR1=${VAR2:-${1}}', 'TESTUNSET'],
+      ['VAR1=${1:-}', 'TESTUNSET'],
+      ['VAR1=${1:-2}', '2'],
+      ['VAR1=${1:-$2}', 'TESTUNSET'],
+
+      // Other forms.
+      ['VAR1=${VAR2-val2}', 'val2'],
+      ['VAR1=${VAR2-$VAR3}', 'VAR3'],
+      ['VAR1=${VAR2-"$VAR3"}', 'VAR3'],
+      ['VAR1=${VAR2-${VAR3}}', 'VAR3'],
+      ['VAR1=${VAR2-"${VAR3}"}', 'VAR3'],
+      ['VAR1=${VAR2:-val2}', 'val2'],
+      ['VAR1=${VAR2-val2}', 'val2'],
+      ['VAR1=${VAR2-${VAR3:-${VAR4-val4}}}', 'val4'],
+      ['VAR1=${VAR2-${VAR3:-${VAR4-}}}', 'VAR4'],
+
+      ['VAR1=${VAR2-${VAR3:-${VAR4?val4}}}', 'VAR4'],
+      ['VAR1=${VAR2-${VAR3:-}}', 'VAR3'],
+      ['VAR1=${VAR2-"${VAR3:-}"}', 'VAR3'],
+
+      ['VAR1=${VAR2:=$VAR3}', 'VAR3'],
+      ['VAR1=${VAR2+=$VAR3}', 'VAR3'],
+      ['VAR1=${VAR2?Some message}', 'VAR2'],
+
+      // Middle of the string.
+      ['VAR1=${VAR1:-./other/${VAR2}/path}', './other/${VAR2}/path'],
+      ['VAR1=${VAR1:-"./other/${VAR2}/path}"', './other/${VAR2}/path'],
+    ];
+  }
+
+  /**
+   * Tests the parseValue() method.
+   *
+   * @dataProvider dataProviderExtractVariableDescription
+   * @covers ::parseDescription
+   */
+  public function testExtractVariableDescription(array $lines, int $line_num, array $skip_prefix, string $expected): void {
+    $actual = VariableParser::parseDescription($lines, $line_num, $skip_prefix);
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for testExtractVariableDescription().
+   */
+  public static function dataProviderExtractVariableDescription(): array {
+    return [
+      [[], 0, [], ''],
+      [[], 10, [], ''],
+      [['string'], 0, [], ''],
+      [['string'], 10, [], ''],
+      [['# first second', 'VAR1'], 1, [], 'first second'],
+      [[' ', '# first second', 'VAR1'], 2, [], 'first second'],
+      [['# zero', ' ', '# first second', 'VAR1'], 3, [], 'first second'],
+      [['# zero', ' ', '# first second', '#', '# third', 'VAR1'], 5, [], "first second\n" . "\n" . 'third'],
+      [['# zero', ' ', '# first second', '#', '# third', '# forth', 'VAR1'], 6, [], "first second\n" . "\n" . "third\n" . 'forth'],
+      //
+      // Description prefixes.
+      [['# zero', ' ', '#;< first second', '# third', '# forth', 'VAR1'], 5, ['#;<'], "third\nforth"],
+      [['# zero', ' ', '#;< first second', '#;> third', '# forth', 'VAR1'], 5, ['#;<', '#;>'], 'forth'],
+      [['# zero', ' ', '#;< first second', '#;> third', '# forth', 'VAR1'], 5, [';<', ';>'], 'forth'],
+      // Special case: removing the skipped prefix should avoid additional line.
+      [['# zero', ' ', '# first second', '#', '#;> third', '# forth', 'VAR1'], 6, [';<', ';>'], "first second\n" . "\n" . 'forth'],
+    ];
+  }
+
+  /**
+   * Tests the validateValue() method.
+   *
+   * @param string $value
+   *   The value to validate.
+   * @param class-string<\Throwable> $expected_exception
+   *   The expected exception class.
+   * @param string $expected_message
+   *   The expected exception message.
+   *
+   * @dataProvider dataProvidervValidateValue
+   * @covers ::validateValue
+   */
+  public function testValidateValue(string $value, ?string $expected_exception, string $expected_message): void {
+    if ($expected_exception) {
+      $this->expectException($expected_exception);
+      $this->expectExceptionMessage($expected_message);
+    }
+    else {
+      $this->expectNotToPerformAssertions();
+    }
+
+    VariableParser::validateValue($value);
+  }
+
+  /**
+   * Data provider for testValidateValue().
+   */
+  public static function dataProvidervValidateValue(): array {
+    return [
+      // Valid values.
+      ['Hello "World"', NULL, ''],
+      ['No special characters', NULL, ''],
+      ['{}', NULL, ''],
+      ['{ "key": "value" }', NULL, ''],
+
+      // Invalid values.
+      ['Hello "World', \RuntimeException::class, 'Invalid number of quotes in the value: Hello "World'],
+      ['{Hello} World}', \RuntimeException::class, 'Unbalanced braces in the value: {Hello} World}'],
+    ];
+  }
+
+}
