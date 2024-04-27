0059c8a4a2fe3f55602b49b194ca50aafee7a540
Alex Skrypnyk <alex@drevops.com>
2024-04-27 11:26:19 +1000

    [#66] Added test for a real example.

---
+++ tests/phpunit/Fixtures/test-data-real.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+##
+# Test data file for variables extraction script.
+#
+# shellcheck disable=SC2034,SC2154
+
+DREVOPS_DRUPAL_PRIVATE_FILES="${DREVOPS_DRUPAL_PRIVATE_FILES:-./${DREVOPS_WEBROOT}/sites/default/files/private}"
